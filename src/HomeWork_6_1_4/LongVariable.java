2cc7d861d6a84b27bff70031b37272346fd67120
ArcBLR <arc11041977@gmail.com>
2024-03-25 23:10:08 +0300

    Создание проекта

--- src/HomeWork_6_1_4/LongVariable.java
+++ src/HomeWork_6_1_4/LongVariable.java
@@ -3,9 +3,13 @@ package HomeWork_6_1_4;
 public class LongVariable {
     public static void main(String[] args) {
         String str = "012312";
-        long str1 = Integer.parseInt(str);
-        System.out.println(str1);
-        long str2 = Integer.valueOf(str);
-        System.out.println(str2);
+        long num1 = Integer.parseInt(str);
+        System.out.println("Способ 1 Integer.parseInt: "+num1);
+        long num2 = Integer.valueOf(str);
+        System.out.println("Способ 2 Integer.valueOf: "+num2);
+        Long num3 = Long.parseLong(str);
+        System.out.println("Способ 3 Long.parseLong: "+num3);
+        Long num4 = Long.valueOf(str);
+        System.out.println("Способ 4 Long.valueOf: "+num4);
     }
 }
