42c5d45c256de23ba7ff6d0a6bd4564c018a2d73
ArcBLR <arc11041977@gmail.com>
2024-03-29 20:48:43 +0300

    небольшие правки ДЗ_6_1_1

--- src/HomeWork_6_1_1/CompareStrings.java
+++ src/HomeWork_6_1_1/CompareStrings.java
@@ -3,13 +3,10 @@ package HomeWork_6_1_1;
 public class CompareStrings {
     public static void main(String[] args) {
         String str1 = "Мама мыла раму";
-        String str2_1 = "Мама";
-        String str2_2 = " мыла ";
-        String str2_3 = "раму";
-        String str2 = str2_1 + str2_2 + str2_3;
+        String str2 = new String("Мама" + " мыла " + "раму");
         System.out.println(str1);
         System.out.println(str2);
-        System.out.println(str1 == str2);
-        System.out.println(str1.equals(str2));
+        System.out.println("сравнение при помощи \"==\" (сравнение ссылок): " + (str1 == str2));
+        System.out.println("сравнение при помощи \"equale\" (сравнение объектов): " + str1.equals(str2));
     }
 }
